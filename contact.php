715b29146c9aab37176e71d7590b8da0951fb8a7
alan_videabiz <alan@videabiz.com>
2021-12-06 18:07:18 +0700

    demo

---
+++ contact.php
@@ -0,0 +1,8 @@
+<a href="https://t.me/YGVentures_Chat" target="_blank" class="inline-flex p-1 bg-gray-700 hover:bg-gray-900 text-white font-normal rounded-full shadow-md transform transition motion-safe:hover:scale-120 duration-500 focus:outline-none h-8 w-8 text-xs">
+    <span class="fa-air m-auto"></span>
+</a>
+<a href="https://twitter.com/YG_Ventures" target="_blank" class="inline-flex p-1 bg-gray-700 hover:bg-gray-900 text-white font-normal rounded-full shadow-md transform transition motion-safe:hover:scale-120 duration-500 focus:outline-none h-8 w-8 text-xs">
+    <span class="fa-bird m-auto"></span>
+</a>
+<a href="" target="_blank" class="inline-flex p-1 bg-gray-700 hover:bg-gray-900 text-white font-normal rounded-full shadow-md transform transition motion-safe:hover:scale-120 duration-500 focus:outline-none h-8 w-8 text-xs">
+    <span class="fa-message m-auto"></span></a>
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YG Ventures</title>
+    <link rel="preload" as="image" href="./image/logo.png">
+    <link rel="icon" href="./image/logo.png" sizes="any" type="image/svg+xml">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/all.css">
+
+</head>
+<body class="min-h-full overflow-x-hidden overflow-y-auto text-white font-serif background-logo" style="" data-aos-easing="ease"
+      data-aos-duration="400" data-aos-delay="0">
+<div id="app" class="overflow-hidden" data-v-app="">
+    <div class="overflow-hidden min-h-screen flex items-center m-auto w-full justify-center">
+        <div class="home-ui">
+            <figure data-aos="fade-down" class="flex flex-col justify-center items-center text-white aos-init aos-animate">
+                <img class="w-64 md:w-80 mb-5" loading="lazy" src="image/Venture1.png" alt="">
+                <figcaption class="text-4xl w-64 md:w-80 flex justify-between hidden"><span class="font-bold mr-3">YG</span><span class="uppercase">Ventures</span></figcaption>
+            </figure>
+            <h2 data-aos="zoom-in"
+                class="__animate__animated __animate__bounce __animate__repeat-3 text-center md:text-3xl sm:text-2xl text-sm font-normal py-10 aos-init aos-animate">
+                Welcome to Vietnam’s Top Incubator</h2>
+            <div data-aos="fade-up"
+                 class="rounded-t-xl overflow-hidden bg-gradient-to-r from-light-blue-50 to-light-blue-100 p-10 aos-init aos-animate">
+                <div class="text-center">
+                    <a href="portfolio.php" style="height: 150px" class="py-3 px-12 bg-aquamarine-500 hover:box-shadow-light text-black font-normal rounded-2xl shadow-md transform transition motion-safe:hover:scale-120 duration-500 focus:outline-none">Portfolio </a>
+                    <a href="partner.php" class="py-3 px-12 bg-aquamarine-500 hover:box-shadow-light text-black font-normal rounded-2xl shadow-md transform transition motion-safe:hover:scale-120 duration-500 focus:outline-none" style="margin-left: 20px">Partner </a>
+                </div>
+            </div>
+            <footer class="text-center flex justify-center space-x-4 fixed bottom-5 left-0 right-0">
+                <?php include_once ("contact.php")?>
+            </footer>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
